dfe937673c76f7f51ec2b4e254fa4904ae642656
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-07-29 21:34:29 +0200

    Added Service with parsing content, CsvHelper dependency

--- ContentParserProject/Program.cs
+++ ContentParserProject/Program.cs
@@ -1,4 +1,6 @@
 using System.Text.Json.Serialization;
+using ContentParserProject.Interfaces;
+using ContentParserProject.Service;
 
 namespace ContentParserProject
 {
@@ -8,6 +10,7 @@ namespace ContentParserProject
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddScoped<IContentParser, ContentParser>();
             builder
                 .Services.AddControllers()
                 .AddJsonOptions(options =>
---
+++ ContentParserProject/Service/ContentParser.cs
@@ -0,0 +1,55 @@
+﻿using ContentParserProject.Interfaces;
+using ContentParserProject.Models;
+using CsvHelper;
+using System.Globalization;
+using System.Text.Json;
+
+namespace ContentParserProject.Service
+{
+    public class ContentParser : IContentParser
+    {
+        public ParseResponse HandleJson(string data)
+        {
+            JsonElement element = JsonSerializer.Deserialize<JsonElement>(data);
+            int count = CountJson(element);
+            return new ParseResponse(Status.Completed, count, element);
+        }
+
+        public ParseResponse HandleCSV(string data)
+        {
+            using var reader = new StringReader(data);
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<dynamic>();
+                JsonElement element = JsonSerializer.SerializeToElement(records);
+                int count = element.GetArrayLength();
+                return new ParseResponse(Status.Completed, count, element);
+            }
+            ;
+        }
+
+        public int CountJson(JsonElement element)
+        {
+            int objCount = 0;
+            if (element.ValueKind == JsonValueKind.Array)
+            {
+                foreach (var item in element.EnumerateArray())
+                {
+                    if (item.ValueKind == JsonValueKind.Object)
+                    {
+                        objCount += CountJson(item);
+                    }
+                }
+            }
+            else if (element.ValueKind == JsonValueKind.Object)
+            {
+                objCount++;
+                foreach (var item in element.EnumerateObject())
+                {
+                    objCount += CountJson(item.Value);
+                }
+            }
+            return objCount;
+        }
+    }
+}
