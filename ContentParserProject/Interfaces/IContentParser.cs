a44e26eaa80fdbf43ea3d6a6edba6ba08cb0775a
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-07-29 21:13:53 +0200

    Added IContentParser interface

---
+++ ContentParserProject/Interfaces/IContentParser.cs
@@ -0,0 +1,12 @@
+﻿using ContentParserProject.Models;
+using System.Text.Json;
+
+namespace ContentParserProject.Interfaces
+{
+    public interface IContentParser
+    {
+        public ParseResponse HandleJson(string data);
+        public ParseResponse HandleCSV(string data);
+        public int CountJson(JsonElement element);
+    }
+}
