78df41cf35fe21d581b3c2d3831f56a8e4a03517
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-07-29 20:58:14 +0200

    Updated ParseRequest model

--- ContentParserProject/Models/ParseRequest.cs
+++ ContentParserProject/Models/ParseRequest.cs
@@ -1,6 +1,17 @@
-﻿namespace ContentParserProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContentParserProject.Models
 {
+    public enum ParsingContentType
+    {
+        CSV,
+        INTERNAL_JSON,
+    };
     public class ParseRequest
     {
+        [EnumDataType(typeof(ParsingContentType))]
+        public required ParsingContentType Type { get; set; }
+
+        public required string Content { get; set; }
     }
 }
